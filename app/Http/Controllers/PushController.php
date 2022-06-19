f3076d78b2c48071c1d0b0cf09e4d94b9d900de6
Cedric Lenders <ceelenders@hotmail.com>
2022-06-19 16:57:18 +0200

    Revert "testing"
    This reverts commit 28254cfb4f080bf09c4bff49d0b0974b94aa2b7e.

--- app/Http/Controllers/PushController.php
+++ app/Http/Controllers/PushController.php
@@ -37,7 +37,7 @@ class PushController extends Controller
     }
 
     public function push(){
-        Notification::send(User::find(1)->get(),new PushDemo);
+        Notification::send(User::all(),new PushDemo);
         return redirect()->back();
     }
 
