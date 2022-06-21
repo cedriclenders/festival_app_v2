d3ebcc69eb906843f6c4a82b63c5cd2f03898369
Cedric Lenders <ceelenders@hotmail.com>
2022-06-21 12:47:17 +0200

    return default if no data

--- config/laravelpwa.php
+++ config/laravelpwa.php
@@ -1,6 +1,5 @@
 <?php
 
-$url = 'https://random-data-api.com/api/address/random_address';
 $url = env('ADMIN_URL').'api/manifest';
 $arrContextOptions=array(
     "ssl"=>array(
@@ -10,65 +9,144 @@ $arrContextOptions=array(
 );
 $response = file_get_contents($url,false, stream_context_create($arrContextOptions));
 $pwaInfo= json_decode($response, true);
-
-
-return [
-    'name' => 'PWA',
-    'manifest' => [
-        'name' => $pwaInfo['name'],
-        'short_name' => $pwaInfo['name'],
+if($pwaInfo)
+{
+    return [
+        'name' => 'PWA',
+        'manifest' => [
+            'name' => $pwaInfo['name'],
+            'short_name' => $pwaInfo['name'],
+            'start_url' => '/',
+            'background_color' => '#ffffff',
+            'theme_color' => '#000000',
+            'display' => 'standalone',
+            'orientation'=> 'any',
+            'status_bar'=> 'black',
+            'icons' => [
+                '72x72' => [
+                    'path' => $pwaInfo['icon'],
+                    'purpose' => 'any'
+                ],
+                '96x96' => [
+                    'path' => $pwaInfo['icon'],
+                    'purpose' => 'any'
+                ],
+                '128x128' => [
+                    'path' => $pwaInfo['icon'],
+                    'purpose' => 'any'
+                ],
+                '144x144' => [
+                    'path' => $pwaInfo['icon'],
+                    'purpose' => 'any'
+                ],
+                '152x152' => [
+                    'path' => $pwaInfo['icon'],
+                    'purpose' => 'any'
+                ],
+                '192x192' => [
+                    'path' => $pwaInfo['icon'],
+                    'purpose' => 'any'
+                ],
+                '384x384' => [
+                    'path' => $pwaInfo['icon'],
+                    'purpose' => 'any'
+                ],
+                '512x512' => [
+                    'path' => $pwaInfo['icon'],
+                    'purpose' => 'any'
+                ],
+            ],
+            'splash' => [
+                '640x1136' => $pwaInfo['icon'],
+                '750x1334' => $pwaInfo['icon'],
+                '828x1792' => $pwaInfo['icon'],
+                '1125x2436' => $pwaInfo['icon'],
+                '1242x2208' => $pwaInfo['icon'],
+                '1242x2688' => $pwaInfo['icon'],
+                '1536x2048' => $pwaInfo['icon'],
+                '1668x2224' => $pwaInfo['icon'],
+                '1668x2388' => $pwaInfo['icon'],
+                '2048x2732' => $pwaInfo['icon'],
+            ],
+            'custom' => []
+        ]
+    ];
+}
+else
+{
+    return [ 
+        'name' => env('APP_NAME', 'My PWA App'),
+        'short_name' => 'PWA',
         'start_url' => '/',
         'background_color' => '#ffffff',
         'theme_color' => '#000000',
         'display' => 'standalone',
-        'orientation'=> 'any',
-        'status_bar'=> 'black',
+        'orientation' => 'any',
+        'status_bar' => 'black',
         'icons' => [
             '72x72' => [
-                'path' => $pwaInfo['icon'],
+                'path' => '/images/icons/icon-72x72.png',
                 'purpose' => 'any'
             ],
             '96x96' => [
-                'path' => $pwaInfo['icon'],
+                'path' => '/images/icons/icon-96x96.png',
                 'purpose' => 'any'
             ],
             '128x128' => [
-                'path' => $pwaInfo['icon'],
+                'path' => '/images/icons/icon-128x128.png',
                 'purpose' => 'any'
             ],
             '144x144' => [
-                'path' => $pwaInfo['icon'],
+                'path' => '/images/icons/icon-144x144.png',
                 'purpose' => 'any'
             ],
             '152x152' => [
-                'path' => $pwaInfo['icon'],
+                'path' => '/images/icons/icon-152x152.png',
                 'purpose' => 'any'
             ],
             '192x192' => [
-                'path' => $pwaInfo['icon'],
+                'path' => '/images/icons/icon-192x192.png',
                 'purpose' => 'any'
             ],
             '384x384' => [
-                'path' => $pwaInfo['icon'],
+                'path' => '/images/icons/icon-384x384.png',
                 'purpose' => 'any'
             ],
             '512x512' => [
-                'path' => $pwaInfo['icon'],
+                'path' => '/images/icons/icon-512x512.png',
                 'purpose' => 'any'
             ],
         ],
         'splash' => [
-            '640x1136' => $pwaInfo['icon'],
-            '750x1334' => $pwaInfo['icon'],
-            '828x1792' => $pwaInfo['icon'],
-            '1125x2436' => $pwaInfo['icon'],
-            '1242x2208' => $pwaInfo['icon'],
-            '1242x2688' => $pwaInfo['icon'],
-            '1536x2048' => $pwaInfo['icon'],
-            '1668x2224' => $pwaInfo['icon'],
-            '1668x2388' => $pwaInfo['icon'],
-            '2048x2732' => $pwaInfo['icon'],
+            '640x1136' => '/images/icons/splash-640x1136.png',
+            '750x1334' => '/images/icons/splash-750x1334.png',
+            '828x1792' => '/images/icons/splash-828x1792.png',
+            '1125x2436' => '/images/icons/splash-1125x2436.png',
+            '1242x2208' => '/images/icons/splash-1242x2208.png',
+            '1242x2688' => '/images/icons/splash-1242x2688.png',
+            '1536x2048' => '/images/icons/splash-1536x2048.png',
+            '1668x2224' => '/images/icons/splash-1668x2224.png',
+            '1668x2388' => '/images/icons/splash-1668x2388.png',
+            '2048x2732' => '/images/icons/splash-2048x2732.png',
+        ],
+        'shortcuts' => [
+            [
+                'name' => 'Shortcut Link 1',
+                'description' => 'Shortcut Link 1 Description',
+                'url' => '/shortcutlink1',
+                'icons' => [
+                    "src" => "/images/icons/icon-72x72.png",
+                    "purpose" => "any"
+                ]
+            ],
+            [
+                'name' => 'Shortcut Link 2',
+                'description' => 'Shortcut Link 2 Description',
+                'url' => '/shortcutlink2'
+            ]
         ],
         'custom' => []
-    ]
-];
+    ];
+}
+
+
